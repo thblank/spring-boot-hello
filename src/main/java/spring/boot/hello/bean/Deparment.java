bba5b242558e74029536a4ed4aeca05d9ad59fb1
tang_hao <18621868375@163.com>
2019-01-09 16:50:31 +0800

    update

--- src/main/java/spring/boot/hello/bean/Deparment.java
+++ src/main/java/spring/boot/hello/bean/Deparment.java
@@ -1,4 +1,33 @@
 package spring.boot.hello.bean;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "deparment")
 public class Deparment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Deparment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
