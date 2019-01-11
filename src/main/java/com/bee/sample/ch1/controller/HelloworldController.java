26c54df13ba81f098f7ace7fb0250231a0efb739
tang_hao <18621868375@163.com>
2019-01-11 15:01:29 +0800

    update

---
+++ src/main/java/com/bee/sample/ch1/controller/HelloworldController.java
@@ -0,0 +1,15 @@
+package com.bee.sample.ch1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloworldController {
+
+    @RequestMapping("/sayHello.html")
+    public @ResponseBody String say(){
+        return "Hello Spring Boot";
+    }
+
+}
