fc9552fb21425271484f3f124e624ee763c0a10d
tang_hao <18621868375@163.com>
2019-04-09 14:47:36 +0800

    update

--- src/main/java/com/bee/sample/ch1/controller/HelloworldController.java
+++ src/main/java/com/bee/sample/ch1/controller/HelloworldController.java
@@ -1,15 +1,24 @@
 package com.bee.sample.ch1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/test")
 public class HelloworldController {
 
-    @RequestMapping("/sayHello.html")
+    @RequestMapping("/${query.all}.json")
     public @ResponseBody String say(){
         return "Hello world Spring Boot";
     }
+
+    @RequestMapping("/index.html")
+    public String sayHello(Model model){
+        model.addAttribute("name","hello,world");
+        return "/index.btl";
+    }
+
 
 }
---
+++ src/main/java/com/bee/sample/ch1/main/StringTestMain.java
@@ -0,0 +1,47 @@
+package com.bee.sample.ch1.main;
+
+import org.springframework.util.StringUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StringTestMain {
+
+    public static void main(String[] args) {
+//        List<String> list = Arrays.asList("Hollis","hollischuang","Java干货");
+//
+//        String s = list.stream().collect(Collectors.joining(",","[","]"));
+//
+//        System.out.println(s);
+//        List<Integer> integers = Arrays.asList(1, 2, 30);
+        Double i = 4.699999809265137;
+        String format = String.format("%.1f", i);
+        System.out.println(format);
+        /*List<Integer> integers1 = integers.subList(0, 2);
+
+        System.out.println(integers1);*/
+
+        String abd = "abd";
+        int num= 20;
+        StringTestMain stringTestMain = new StringTestMain();
+
+        if (stringTestMain == null) {
+            if (stringTestMain != null) {
+                if (stringTestMain != null) {
+                    
+                }
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (Integer integer : list) {
+            
+        }
+        boolean flag = true;
+        if (!flag) {
+
+        }
+
+
+    }
+}
