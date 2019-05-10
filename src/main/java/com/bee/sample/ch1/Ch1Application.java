74c78c57f42319a7a3f711c392a8b47d31a8938b
tang_hao <18621868375@163.com>
2019-05-10 16:29:33 +0800

    取消热部署

--- src/main/java/com/bee/sample/ch1/Ch1Application.java
+++ src/main/java/com/bee/sample/ch1/Ch1Application.java
@@ -5,12 +5,19 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @SpringBootApplication
 @EnableAutoConfiguration
 public class Ch1Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+//        System.setProperty("java.awt.headless", "false");
         SpringApplication.run(Ch1Application.class,args);
+//        Desktop.getDesktop().browse(new URI("http://127.0.0.1:8080/swagger-ui.html"));
     }
 
 }
